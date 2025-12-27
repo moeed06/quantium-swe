b3351830d0ccbf8e3f5672c7e692ccfb35d2b129
Nitrate45 <moeedryousaf@outlook.com>
2025-12-27 23:36:01 +0000

    Task 2
    Creates a cleaned CSV file with the date, region and new "sales" field

---
+++ project.py
@@ -0,0 +1,32 @@
+import csv
+salesString = r"data\daily_sales_data_"
+headerWritten = False
+
+
+for i in range(0,2):
+    newSalesString = salesString + str(i) + ".csv"
+
+    with open(newSalesString) as csvfile_input, open("output.csv", "w", newline="") as csvfile_output:
+        csv_input = csv.reader(csvfile_input)
+        csv_output = csv.writer(csvfile_output)
+        
+        headerArray = csv_input.__next__()
+        headerArray.append("sales")
+        headerArray.remove("product")
+        headerArray.remove("price")
+        headerArray.remove("quantity")
+        csv_output.writerow(headerArray)
+
+        for row in csv_input:
+            if row[0] == "pink morsel":
+                row.append("$" + str(float(row[1].strip('$')) * int(row[2])))
+                row.pop(0)
+                row.pop(0)
+                row.pop(0)
+                print(row)
+                csv_output.writerow(row)
+
+with open("output.csv") as f:
+    csv_reading = csv.reader(f)
+    for row in csv_reading:
+        print(row)
